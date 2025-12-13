c52a3814124d5bd353adf5b103206dc9101f99ed
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2025-12-13 15:19:57 +0600

    learning deepagents framework

---
+++ deepagent-test/main.py
@@ -0,0 +1,73 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
+import asyncio
+from typing import Literal
+from deepagents import create_deep_agent
+from langchain_openai import ChatOpenAI
+from deepagents.backends import CompositeBackend, StateBackend, StoreBackend
+from langgraph.store.postgres.aio import AsyncPostgresStore
+from tavily import TavilyClient
+
+
+model = ChatOpenAI(
+    model="gpt-5.2",
+    reasoning={
+        "effort": "low",
+        "summary": "auto",
+    },
+    use_responses_api=True,
+)
+tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+
+def internet_search(
+    query: str,
+    max_results: int = 5,
+    topic: Literal["general", "news", "finance"] = "general",
+    include_raw_content: bool = False,
+):
+    """Run a web search"""
+    return tavily_client.search(
+        query,
+        max_results=max_results,
+        include_raw_content=include_raw_content,
+        topic=topic,
+    )
+
+
+def make_backend(runtime):
+    return CompositeBackend(
+        default=StateBackend(runtime),  # Ephemeral storage
+        routes={
+            "/memories/": StoreBackend(runtime)  # Persistent storage
+        },
+    )
+
+
+async def main():
+    async with AsyncPostgresStore.from_conn_string(os.environ["DATABASE_URL"]) as store:
+        await store.setup()
+
+        agent = create_deep_agent(
+            store=store,
+            backend=lambda rt: CompositeBackend(
+                default=StateBackend(rt),
+                routes={"/memories/": StoreBackend(rt)},
+            ),
+            system_prompt="""
+You are a generic terminal agent. Your primary goal is to assist the user in their productivity tasks.
+
+Your persistent memory structure:
+- /memories/preferences.txt: User preferences and settings
+- /memories/context/: Long-term context about the user
+- /memories/knowledge/: Facts and information learned over time
+""".strip(),
+            tools=[internet_search],
+        )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
