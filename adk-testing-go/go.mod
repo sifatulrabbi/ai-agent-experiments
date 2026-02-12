5b026102b96c42b84fc08498cc3807d4e016118e
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2026-02-12 19:55:29 +0600

    testing out google adk

---
+++ adk-testing-go/go.mod
@@ -0,0 +1,3 @@
+module adk-testing-go
+
+go 1.25.0
---
+++ adk-testing/test_agent/agent.py
@@ -0,0 +1,10 @@
+from google.adk.agents.llm_agent import Agent
+
+
+root_agent = Agent(
+    model="gemini-3-flash-preview",
+    name="root_agent",
+    description="A helpful assistant for user questions.",
+    instruction="Answer user questions to the best of your knowledge",
+    tools=[],
+)
